0bdee988e8e0daec3e258b61372408bcce00e05a
Eric <34190213+epodol@users.noreply.github.com>
2020-12-20 17:23:57 -0700

    Register serviceWorker.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,6 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
