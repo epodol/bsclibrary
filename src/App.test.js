6966730b0c7fd9385b2b46268dece4782b1c8f2f
gazcpp <14085418+gazcpp@users.noreply.github.com>
2021-01-01 21:25:33 -0700

    Fix unit test

--- src/App.test.js
+++ src/App.test.js
@@ -4,13 +4,11 @@ import { act } from 'react-dom/test-utils';
 
 let container = null;
 beforeEach(() => {
-  // setup a DOM element as a render target
   container = document.createElement('div');
   document.body.appendChild(container);
 });
 
 afterEach(() => {
-  // cleanup on exiting
   unmountComponentAtNode(container);
   container.remove();
   container = null;
