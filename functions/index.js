0b539f148721358f1182d15fd16740449b0f0a5d
Eric <34190213+epodol@users.noreply.github.com>
2021-03-14 20:36:59 -0700

    Merge main into epodol/add-book-features

--- functions/index.js
+++ functions/index.js
@@ -3,19 +3,13 @@ const admin = require('firebase-admin');
 const setRole = require('./src/setRole');
 const setBookQueryData = require('./src/setBookQueryData');
 const addNewUser = require('./src/addNewUser');
-<<<<<<< HEAD
 
 const setGenesisUser = require('./src/setGenesisUser');
-=======
->>>>>>> main
 
 admin.initializeApp();
 
 exports.setRole = setRole.setRole;
 exports.setBookQueryData = setBookQueryData.setBookQueryData;
 exports.addNewUser = addNewUser.addNewUser;
-<<<<<<< HEAD
 
 exports.setGenesisUser = setGenesisUser.setGenesisUser;
-=======
->>>>>>> main
