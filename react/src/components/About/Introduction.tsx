44e2450dd75861696995747108856ed0e94df572
Abhinav Raghavan <87729988+akraghavan@users.noreply.github.com>
2021-07-20 15:47:55 -0700

    Fix formatting

--- react/src/components/About/Introduction.tsx
+++ react/src/components/About/Introduction.tsx
@@ -7,9 +7,11 @@ const Introduction = () => (
     <p>
       Our goal is to gather books and create an area where all BASIS Scottsdale
       students can come check out books for their enjoyment. This will also
-      serve as a resource center for students to acquire more knowledge about their fields of study.
-      The library will be designed in a way where everyone will be be able to check out books in a reasonable time frame to ensure that
-      all students have equal access to our catalogue! We hope to see you there!
+      serve as a resource center for students to acquire more knowledge about
+      their fields of study. The library will be designed in a way where
+      everyone will be be able to check out books in a reasonable time frame to
+      ensure that all students have equal access to our catalogue! We hope to
+      see you there!
     </p>
   </Container>
 );
