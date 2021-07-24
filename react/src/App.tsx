1041338b85f15f9b85bf333737c59844bef540b1
Eric <34190213+epodol@users.noreply.github.com>
2021-07-23 18:09:20 -0700

    Fix Google Analytics Environment Variable

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -32,9 +32,9 @@ const firebaseConfig = {
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const preloadSDKs = (firebaseApp: any) => {
