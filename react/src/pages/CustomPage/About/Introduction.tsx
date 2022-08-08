6d7939b00ad274f4a005111b497b05929ae58a72
epodol <34190213+epodol@users.noreply.github.com>
2022-08-08 13:34:59 -0700

    Fix Typos and Bugs (#740)

--- react/src/pages/CustomPage/About/Introduction.tsx
+++ react/src/pages/CustomPage/About/Introduction.tsx
@@ -9,7 +9,7 @@ const Introduction = () => (
       students can come check out books for their enjoyment. This will also
       serve as a resource center for students to acquire more knowledge about
       their fields of study. The library will be designed in a way where
-      everyone will be be able to check out books for a reasonable time frame to
+      everyone will be able to check out books for a reasonable time frame to
       ensure that all students have equal access to our catalog! We hope to see
       you there!
     </p>
